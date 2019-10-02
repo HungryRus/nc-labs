9ae22134cc0ab6c4d91f7b90fe7640c014e3cea3
HungryRus <hungry163@yandex.ru>
2019-10-02 21:29:43 +0400

    lab0 is done

---
+++ lab0/Task5/MyFirstClass.java
@@ -0,0 +1,25 @@
+import myfirstpackage.*;
+
+public class MyFirstClass {
+
+	public static void main(String[] s) {
+		MySecondClass o = new MySecondClass(5, 5);
+		
+		int i, j;
+		
+		for (i = 1; i <= 8; i++) {
+			for(j = 1; j <= 8; j++) {
+				o.setA(i);
+				o.setB(j);
+
+				System.out.print(o.sum());
+
+				System.out.print(" ");
+			}
+			System.out.println();
+		}
+	}
+} 	
+
+
+
